52de895db6abe2cc30d1b0b5cef473ab9a6d9448
cou256 <cou256>
2017-07-05 15:16:28 +0900

    最初のコミット

---
+++ ProcessMonitoring/MainMonitoring.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ProcessMonitoring
+{
+    using CAUtility;
+    /// <summary>
+    /// プロセス監視
+    /// </summary>
+    public class MainMonitoring
+    {
+        const String Xml = "ProcessMonitoringSetting.xml";
+        public MainMonitoring()
+        {
+            var setting = XmlSetting.Load<Setting>(Xml);
+            Run(setting.FileName, setting.Options, setting.MonitoringInterval);
+        }
+        void Run(String fileName, String arguments, int monitoringInterval)
+        {
+            var process = Process.Start(fileName, arguments);
+            process.EnableRaisingEvents = true;
+            process.Exited += (e, a) => Run(fileName, arguments, monitoringInterval);
+            while (true)
+            {
+                SwitchToThisWindow(process.MainWindowHandle, true);
+                Thread.Sleep(monitoringInterval);
+            }
+        }
+        static void Main()
+        {
+            new MainMonitoring();
+        }
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+        public class Setting
+        {
+            public String FileName;
+            public String Options;
+            public int MonitoringInterval;
+        }
+    }
+}
---
+++ ProcessMonitoring/XmlSetting.cs
@@ -0,0 +1,52 @@
+﻿namespace CAUtility
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    /// <summary>
+    ///  XMLを読み込みシリアライズ/デシリアライズを行います。
+    /// </summary>
+    public sealed class XmlSetting
+    {
+        /// <summary>エンコード</summary>
+        private static UTF8Encoding utf = new UTF8Encoding(false);
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private XmlSetting()
+        {
+            throw new NotImplementedException("インスタンス化できません。");
+        }
+        /// <summary>
+        /// XMLロード
+        /// </summary>
+        /// <typeparam name="T">マッピングするクラスタイプ</typeparam>
+        /// <param name="filePath">ファイルパス</param>
+        /// <returns>マッピングされたインスタンス</returns>
+        public static T Load<T>(string filePath)
+        {
+            using (var streamReader = new StreamReader(filePath, utf))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(T));
+                return (T)xmlSerializer.Deserialize(streamReader);
+            }
+        }
+        /// <summary>
+        /// XMLにセーブ
+        /// </summary>
+        /// <typeparam name="T">マッピングするクラスタイプ</typeparam>
+        /// <param name="filePath">ファイルパス</param>
+        /// <param name="instance">XML化するインスタンス</param>
+        public static void Save<T>(string filePath, T instance)
+        {
+            using (var streamWriter = new StreamWriter(filePath, true, utf))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(T));
+                xmlSerializer.Serialize(streamWriter, instance);
+            }
+        }
+    }
+}
